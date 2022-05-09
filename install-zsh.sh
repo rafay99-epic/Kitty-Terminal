7e62c2d31bbc5fb0c2e9ebf48d14eb0fe9180554
Abdul Rafay <82662797+rafay99-epic@users.noreply.github.com>
2022-05-09 11:52:44 +0500

    install paru

--- install-zsh.sh
+++ install-zsh.sh
@@ -101,7 +101,11 @@ function arch()
            Installing Paru
 -------------------------------------------------------------------------
 "
-    sudo pacman -S paru --noconfirm --needed    
+    sudo pacman -S --needed base-devel
+    git clone https://aur.archlinux.org/paru.git
+    cd paru
+    makepkg -si
+    cd ../  
 
     echo -ne "
 -------------------------------------------------------------------------
